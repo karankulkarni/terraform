5509e40e0ab539888f94081d9c97056fdb1fce6f
karankulkarni <79139704+karankulkarni@users.noreply.github.com>
2021-09-24 12:56:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_storage_account" "store" {
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     appid = var.appid
